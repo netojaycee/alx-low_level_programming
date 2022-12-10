c505b444c751cbb73438ab733b2074d9758005ba
Edeh John Chinonso <netojayee@gmail.com>
2022-12-09 20:57:38 -0800

    f

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+  
+{
+  
+  int nums;
+  
+  int nums1;
+
+  
+  for (nums = '1'; nums <= '9'; nums++)
+    
+    {
+      
+      for (nums1 = 0; nums1 <= '9'; nums1++)
+	
+	{	      
+	      putchar(nums);
+	      
+	      putchar(nums1);
+	      
+	      
+	      if (nums == '9' && nums1 == '9')
+		
+		break;
+	      
+	      putchar(',');
+	      
+	      putchar(' ');
+	      
+	    }
+	  
+	}
+      
+    }
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
